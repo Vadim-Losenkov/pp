b75ce358097362a135c28ddc1c11ca554ec8f1a0
Vadim Losenkov <vprog0729@gmail.com>
2022-03-28 20:45:47 +0300

    _

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
 $(function() {
   $('[data-form="number"]').mask('+7 (999) 999-9999');
 })
+
+for (let i = 1; i <= 5; i++) {
+  new Swiper(`.category-${i} .portfolio__slider`, {
+    spaceBetween: 30,
+    pagination: {
+      el: `.portfolio__pagination-${i} .portfolio__pagination-num`,
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: `.portfolio__pagination-${i} .portfolio__pagination-arrow--right`,
+      prevEl: `.portfolio__pagination-${i} .portfolio__pagination-arrow--left`,
+    },
+  })
+}
+
+mixitup('.portfolio__inner', {
+  load: {
+    filter: '.category-1'
+  }
+})
