1dc0753330fffa8454fe988d082870b065f265e7
Вадим Лосенков <vProg0729@gmail.com>
2022-03-29 11:39:09 +0300

    start adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,27 @@
-$(function() {
+$(function () {
   $('[data-form="number"]').mask('+7 (999) 999-9999');
-})
+  $('.header__burger').on('click', function () {
+    $(this).toggleClass('active');
+    $('.mobile-menu').toggleClass('active');
+  });
+});
 
 for (let i = 1; i <= 5; i++) {
   new Swiper(`.category-${i} .portfolio__slider`, {
     spaceBetween: 30,
     pagination: {
       el: `.portfolio__pagination-${i} .portfolio__pagination-num`,
-      type: "fraction",
+      type: 'fraction',
     },
     navigation: {
       nextEl: `.portfolio__pagination-${i} .portfolio__pagination-arrow--right`,
       prevEl: `.portfolio__pagination-${i} .portfolio__pagination-arrow--left`,
     },
-  })
+  });
 }
 
 mixitup('.portfolio__inner', {
   load: {
-    filter: '.category-1'
-  }
-})
+    filter: '.category-1',
+  },
+});
